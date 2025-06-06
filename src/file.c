53406a9c6b51085a536b177cea42fa2a75e602fc
rphii <raphael.seitz18@gmail.com>
2025-06-06 22:49:14 +0000

    fixed

--- src/file.c
+++ src/file.c
@@ -167,7 +167,6 @@ ErrDecl file_exec(Str path, VStr *subdirs, bool recursive, bool hidden, FileFunc
     Str dot = str(".");
     Str dotdot = str("..");
     if(!str_len(path)) return 0;
-    printff("FILENAME: %.*s", STR_F(path));
     FileTypeList type = file_get_type(path);
     vstr_free_set(subdirs);
     Str filename = {0};
@@ -200,7 +199,6 @@ ErrDecl file_exec(Str path, VStr *subdirs, bool recursive, bool hidden, FileFunc
                 array_push(*subdirs, filename);
                 str_zero(&filename);
             } else if(type2 == FILE_TYPE_FILE) {
-                printff("FILENAME: %.*s", STR_F(filename));
                 TRY(exec(filename, args), "an error occured while executing the function");
                 str_clear(&filename);
             } else {
