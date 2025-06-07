9ffff5cd55805badd4f75f06fcd7af1dc30576ad
rphii <raphael.seitz18@gmail.com>
2025-06-07 00:27:58 +0000

    add color_fmt_rgb

---
+++ src/color.c
@@ -0,0 +1,13 @@
+#include "color.h"
+#include "str.h"
+
+void color_fmt_rgb(Str *out, Color color) { /*{{{*/
+    ASSERT_ARG(out);
+    if(!str_is_dynamic(*out)) ABORT("attempting to format constant string");
+    Str text = STR_DYN();
+    str_fmt(&text, "#%2x%2x%2x", color.r, color.g, color.b);
+    str_fmt_fgbg(out, text, COLOR_NONE, color, false, false, false);
+    str_free(&text);
+} /*}}}*/
+
+
