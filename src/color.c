41eab6765719b927a6650a53b8feeefb91810ffd
rphii <raphael.seitz18@gmail.com>
2025-06-07 00:30:22 +0000

    fix spacing

--- src/color.c
+++ src/color.c
@@ -5,7 +5,7 @@ void color_fmt_rgb(Str *out, Color color) { /*{{{*/
     ASSERT_ARG(out);
     if(!str_is_dynamic(*out)) ABORT("attempting to format constant string");
     Str text = STR_DYN();
-    str_fmt(&text, "#%2x%2x%2x", color.r, color.g, color.b);
+    str_fmt(&text, "#%02x%02x%02x", color.r, color.g, color.b);
     str_fmt_fgbg(out, text, COLOR_NONE, color, false, false, false);
     str_free(&text);
 } /*}}}*/
