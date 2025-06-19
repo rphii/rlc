e97a38122d43605032c53a2851a35c264b2be1fa
rphii <raphael.seitz18@gmail.com>
2025-06-19 23:45:00 +0200

    move rgba to the front

--- src/color.h
+++ src/color.h
@@ -4,13 +4,13 @@
 
 typedef struct Color {
     union {
+        uint32_t rgba;
         struct {
             uint8_t r;
             uint8_t g;
             uint8_t b;
             uint8_t a;
         };
-        uint32_t rgba;
     };
 } Color;
 
