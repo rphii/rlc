56368de68e33d90af0840574b0908e2f628160eb
rphii <raphael.seitz18@gmail.com>
2025-06-20 00:04:23 +0200

    fix this

--- src/str.c
+++ src/str.c
@@ -1065,12 +1065,14 @@ void str_fmtx(Str *out, StrFmtX fmtx, char *fmt, ...) {
     va_end(va);
     size_t len_new = out->len;
     str_fmt_fgbg(out, str_i0(*out, len_old), fmtx.fg, fmtx.bg, fmtx.bold, fmtx.italic, fmtx.underline);
-    size_t len_diff = len_new - len_old;
-    printff("FINAL STRING:[%.*s]", STR_F(*out));
+    size_t len_diff = out->len - len_new;
+    printff("len old %zu, new %zu, diff %zu", len_old, len_new, len_diff);
+    printff("PART STRING:[%.*s]:%zu", STR_F(*out), out->len);
     printff("MOVE: %p -> %p x %zu", str_it(*out, len_new), str_it(*out, len_old), len_diff);
-    memmove(str_it(*out, len_new), str_it(*out, len_old), len_diff);
-    out->len -= len_diff;
+    memmove(str_it(*out, len_old), str_it(*out, len_new), len_diff);
+    out->len -= (len_new - len_old);
     array_resize(out, out->len + 1);
+    printff("FINAL STRING:[%.*s]:%zu", STR_F(*out), out->len);
 }
 
 void str_input(Str *str) { /*{{{*/
