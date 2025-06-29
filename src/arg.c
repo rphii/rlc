6e8d4926901ca209b13d15fdf62662842cb63c28
rphii <raphael.seitz18@gmail.com>
2025-06-29 22:56:50 +0200

    newline, comma, etc

--- src/arg.c
+++ src/arg.c
@@ -688,7 +688,8 @@ void argx_fmt_val(Str *out, Arg *arg, ArgX *x, ArgXVal val, StrC prefix) {
             str_fmtx(out, arg->fmt.val_delim, "%.*s", STR_F(prefix));
             str_fmtx(out, arg->fmt.val_delim, "[");
             for(size_t i = 0; i < array_len(*val.v); ++i) {
-                str_fmtx(out, arg->fmt.val_delim, "\n");
+                if(x) str_fmtx(out, arg->fmt.val_delim, ",");
+                str_push(out, '\n');
                 Str s = array_at(*val.v, i);
                 str_fmtx(out, arg->fmt.val, "%.*s", STR_F(s));
             }
