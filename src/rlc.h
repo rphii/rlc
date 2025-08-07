26c4b18116a7f36f43f1c0a8293976d8b7e1ff27
rphii <raphael.seitz18@gmail.com>
2025-08-07 19:53:19 +0200

    move around

---
+++ src/rlc.h
@@ -0,0 +1,15 @@
+#ifndef RLC_H
+
+#include "rlc/array.h"
+#include "rlc/attr.h"
+#include "rlc/color.h"
+#include "rlc/colorprint.h"
+#include "rlc/endian-detect.h"
+#include "rlc/err.h"
+#include "rlc/lut.h"
+#include "rlc/platform-detect.h"
+#include "rlc/vec.h"
+
+#define RLC_H
+#endif
+
