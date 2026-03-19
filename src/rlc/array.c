d2fc77865f92cca655c9fb88b456167536665028
rphii <raphael.seitz18@gmail.com>
2026-03-19 21:05:00 +0000

    fix prefix on array

--- src/rlc/array.c
+++ src/rlc/array.c
@@ -7,49 +7,49 @@
 #include <stdio.h>
 #include <rlc/err.h>
 
-static inline void *array_init();
-static inline void *_array_grow2(void *array, size_t size, size_t capacity);
+static inline void *rl_array_init();
+static inline void *_rl_array_grow2(void *array, size_t size, size_t capacity);
 
-#define array_assert_arg(arg)   ASSERT(arg, "null pointer argument!");
-#define array_base(array)       ((array) - offsetof(Array, data))
+#define rl_array_assert_arg(arg)   ASSERT(arg, "null pointer argument!");
+#define rl_array_base(array)       ((array) - offsetof(RL_Array, data))
 
-#define array_error(msg, ...)     do { \
+#define rl_array_error(msg, ...)     do { \
         ABORT("\n" "array error: " msg "\n", ##__VA_ARGS__); \
     } while(0)
 
-typedef struct Array {
+typedef struct RL_Array {
     size_t length;
     size_t capacity;
     void *data;
-} Array;
+} RL_Array;
 
-static inline void *array_init() {
-    Array *v = malloc(sizeof(Array));
+static inline void *rl_array_init() {
+    RL_Array *v = malloc(sizeof(RL_Array));
     if(!v) {
-        array_error("failed creating array");
+        rl_array_error("failed creating array");
     }
     memset(v, 0, sizeof(*v));
     return &v->data;
 }
 
-static inline void *_array_grow2(void *array, size_t size, size_t capacity) {
+static inline void *_rl_array_grow2(void *array, size_t size, size_t capacity) {
     if(!array) {
-        array = array_init();
+        array = rl_array_init();
     }
-    Array *v = array_base(array);
+    RL_Array *v = rl_array_base(array);
     if(capacity <= v->capacity) return array;
     if(capacity * 2 < capacity) {
-        array_error("invalid allocation size: %zu", capacity);
+        rl_array_error("invalid allocation size: %zu", capacity);
     }
     size_t require = 2;
     while(require < capacity) require *= 2;
-    size_t bytes = sizeof(Array) + size * require;
-    if((bytes - sizeof(Array)) / size != require) {
-        array_error("array member of %zu bytes can't allocate %zu elements", size, require);
+    size_t bytes = sizeof(RL_Array) + size * require;
+    if((bytes - sizeof(RL_Array)) / size != require) {
+        rl_array_error("array member of %zu bytes can't allocate %zu elements", size, require);
     }
     void *temp = realloc(v, bytes);
     if(!temp) {
-        array_error("failed allocation of: %zu elements (%zu bytes)", require, bytes);
+        rl_array_error("failed allocation of: %zu elements (%zu bytes)", require, bytes);
     }
     v = temp;
     memset((unsigned char *)&v->data + size * v->capacity, 0, size * (require - v->capacity));
@@ -57,113 +57,113 @@ static inline void *_array_grow2(void *array, size_t size, size_t capacity) {
     return &v->data;
 }
 
-void _array_grow(void *array, size_t size, size_t capacity) {
-    array_assert_arg(array);
+void _rl_array_grow(void *array, size_t size, size_t capacity) {
+    rl_array_assert_arg(array);
     void **p = array;
-    *p = _array_grow2(*p, size, capacity);
+    *p = _rl_array_grow2(*p, size, capacity);
 }
 
-void _array_resize(void *array, size_t size, size_t length) {
-    array_assert_arg(array);
+void _rl_array_resize(void *array, size_t size, size_t length) {
+    rl_array_assert_arg(array);
     void **p = array;
-    *p = _array_grow2(*p, size, length);
-    Array *v = array_base(*p);
+    *p = _rl_array_grow2(*p, size, length);
+    RL_Array *v = rl_array_base(*p);
     v->length = length;
 }
 
-void *_array_copy(void *array, size_t size) {
+void *_rl_array_copy(void *array, size_t size) {
     if(!array) return 0;
-    void *v = array_init();
-    size_t len = array_len(array);
-    _array_grow(&v, size, len);
+    void *v = rl_array_init();
+    size_t len = rl_array_len(array);
+    _rl_array_grow(&v, size, len);
     memcpy(v, array, size * len);
     return v;
 }
 
-void *_array_addr(const void *array, size_t size, size_t index) {
-    array_assert_arg(array);
+void *_rl_array_addr(const void *array, size_t size, size_t index) {
+    rl_array_assert_arg(array);
 #if !defined(NDEBUG)
-    Array *v = (void *)array_base(array);
+    RL_Array *v = (void *)rl_array_base(array);
     if(!(index < v->length)) {
-        array_error("index %zu is out of bounds %zu", index, v->length);
+        rl_array_error("index %zu is out of bounds %zu", index, v->length);
     }
 #endif
     return (unsigned char *)array + size * index;
 }
 
-void *_array_push(void *array, size_t size) {
-    void **p = array; Array *v = *p ? array_base(*p) : 0;
-    *p = _array_grow2(*p, size, v ? v->length + 1 : 1);
-    v = array_base(*p);
+void *_rl_array_push(void *array, size_t size) {
+    void **p = array; RL_Array *v = *p ? rl_array_base(*p) : 0;
+    *p = _rl_array_grow2(*p, size, v ? v->length + 1 : 1);
+    v = rl_array_base(*p);
     size_t index = v->length++;
     return (unsigned char *)&v->data + size * index;
 }
 
-void *_array_pop(void *array, size_t size) {
-    array_assert_arg(array);
-    Array *v = array_base(array);
+void *_rl_array_pop(void *array, size_t size) {
+    rl_array_assert_arg(array);
+    RL_Array *v = rl_array_base(array);
 #if !defined(NDEBUG)
     if(!v->length) {
-        array_error("no elements left to pop");
+        rl_array_error("no elements left to pop");
     }
 #endif
     size_t index = --v->length;
     return array + size * index;
 }
 
-void _array_free_index(Array *v, size_t size, size_t index, Array_Free f) {
+void _rl_array_free_index(RL_Array *v, size_t size, size_t index, RL_Array_Free f) {
     if(!size) return;
-    array_assert_arg(size);
+    rl_array_assert_arg(size);
     void *val = (unsigned char *)&v->data + size * index;
     if(!val) return;
     if(f) f(val);
 }
 
-void _array_free(void *array) {
-    array_assert_arg(array);
+void _rl_array_free(void *array) {
+    rl_array_assert_arg(array);
     void **p = array;
     if(!*p) return;
-    Array *v = array_base(*p);
+    RL_Array *v = rl_array_base(*p);
     free(v);
     *p = 0;
 }
 
-void _array_free_ext(void *array, size_t size, Array_Free f) {
-    array_assert_arg(array);
-    array_assert_arg(f);
+void _rl_array_free_ext(void *array, size_t size, RL_Array_Free f) {
+    rl_array_assert_arg(array);
+    rl_array_assert_arg(f);
     void **p = array;
     if(!*p) return;
-    Array *v = array_base(*p);
+    RL_Array *v = rl_array_base(*p);
     for(size_t index = 0; index < v->length; ++index) {
-        _array_free_index(v, size, index, f);
+        _rl_array_free_index(v, size, index, f);
     }
     free(v);
     *p = 0;
 }
 
-size_t _array_len(const void *array) {
+size_t _rl_array_len(const void *array) {
     if(!array) return 0;
-    Array *v = (Array *)array_base(array);
+    RL_Array *v = (RL_Array *)rl_array_base(array);
     return v->length;
 }
 
-size_t _array_cap(const void *array) {
+size_t _rl_array_cap(const void *array) {
     if(!array) return 0;
-    Array *v = (Array *)array_base(array);
+    RL_Array *v = (RL_Array *)rl_array_base(array);
     return v->capacity;
 }
 
-void _array_clear(void *array) {
+void _rl_array_clear(void *array) {
     if(!array) return;
-    Array *v = array_base(array);
+    RL_Array *v = rl_array_base(array);
     v->length = 0;
 }
 
-void _array_clear_ext(void *array, size_t size, Array_Free f) {
+void _rl_array_clear_ext(void *array, size_t size, RL_Array_Free f) {
     if(!array) return;
-    Array *v = array_base(array);
+    RL_Array *v = rl_array_base(array);
     for(size_t index = 0; index < v->length; ++index) {
-        _array_free_index(v, size, index, f);
+        _rl_array_free_index(v, size, index, f);
     }
     v->length = 0;
 }
--- src/rlc/array.h
+++ src/rlc/array.h
@@ -2,7 +2,7 @@
 
 #include <stddef.h>
 
-typedef void (*Array_Free)(void *);
+typedef void (*RL_Array_Free)(void *);
 
 #define ARRAY_FREE(T)   \
     typeof((void)(*)(T))
@@ -10,49 +10,49 @@ typedef void (*Array_Free)(void *);
 /* functions for use {{{ */
 
 /* take address automatically */
-#define array_grow(array, capacity)     _array_grow(&array, sizeof(*array), capacity)
-#define array_resize(array, length)     _array_resize(&array, sizeof(*array), length)
-#define array_push(array, item)         (*(typeof(array))_array_push(&array, sizeof(*array)) = item)
-#define array_free(array)               _array_free(&array)
-#define array_free_ext(array, f)        _array_free_ext(&array, sizeof(*array), (Array_Free)(void(*)(typeof(array)))(f))
+#define rl_array_grow(array, capacity)     _rl_array_grow(&array, sizeof(*array), capacity)
+#define rl_array_resize(array, length)     _rl_array_resize(&array, sizeof(*array), length)
+#define rl_array_push(array, item)         (*(typeof(array))_rl_array_push(&array, sizeof(*array)) = item)
+#define rl_array_free(array)               _rl_array_free(&array)
+#define rl_array_free_ext(array, f)        _rl_array_free_ext(&array, sizeof(*array), (RL_Array_Free)(void(*)(typeof(array)))(f))
 
 /* don't take address */
-#define array_copy(array)               _array_copy(array, sizeof(*array))
-#define array_pop(array)                *(typeof(array))_array_pop(array, sizeof(*array))
-#define array_at(array, index)          *(typeof(array))_array_addr(array, sizeof(*array), index)
-#define array_it(array, index)          (typeof(array))_array_addr(array, sizeof(*array), index)
-#define array_len(array)                _array_len(array)
-#define array_cap(array)                _array_cap(array)
-#define array_clear(array)              _array_clear(array)
-#define array_clear_ext(array, f)       _array_clear_ext(array, sizeof(*array), (Array_Free)(void(*)(typeof(array)))(f))
-#define array_itE(array)                (array ? (typeof(array))array + _array_len(array) : 0)
-#define array_itL(array)                (typeof(array))_array_addr(array, sizeof(*array), _array_len(array) - 1)
-#define array_atL(array)                *(typeof(array))_array_addr(array, sizeof(*array), _array_len(array) - 1)
+#define rl_array_copy(array)               _rl_array_copy(array, sizeof(*array))
+#define rl_array_pop(array)                *(typeof(array))_rl_array_pop(array, sizeof(*array))
+#define rl_array_at(array, index)          *(typeof(array))_rl_array_addr(array, sizeof(*array), index)
+#define rl_array_it(array, index)          (typeof(array))_rl_array_addr(array, sizeof(*array), index)
+#define rl_array_len(array)                _rl_array_len(array)
+#define rl_array_cap(array)                _rl_array_cap(array)
+#define rl_array_clear(array)              _rl_array_clear(array)
+#define rl_array_clear_ext(array, f)       _rl_array_clear_ext(array, sizeof(*array), (RL_Array_Free)(void(*)(typeof(array)))(f))
+#define rl_array_itE(array)                (array ? (typeof(array))array + _rl_array_len(array) : 0)
+#define rl_array_itL(array)                (typeof(array))_rl_array_addr(array, sizeof(*array), _rl_array_len(array) - 1)
+#define rl_array_atL(array)                *(typeof(array))_rl_array_addr(array, sizeof(*array), _rl_array_len(array) - 1)
 
 /*}}}*/
 
 /* macros {{{ */
-#define array_extend(array, other)      do { \
-            for(typeof(array) it_array_internal = other; it_array_internal < array_itE(other); ++it_array_internal) { \
-                array_push(array, *it_array_internal); \
+#define rl_array_extend(array, other)      do { \
+            for(typeof(array) it_array_internal = other; it_array_internal < rl_array_itE(other); ++it_array_internal) { \
+                rl_array_push(array, *it_array_internal); \
             } \
         } while(0)
 /*}}}*/
 
 /* internal functions {{{ */
 
-void _array_grow(void *array, size_t size, size_t capacity);
-void _array_resize(void *array, size_t size, size_t length);
-void *_array_copy(void *array, size_t size);
-void *_array_push(void *array, size_t size);
-void *_array_pop(void *array, size_t size);
-void *_array_addr(const void *array, size_t size, size_t index);
-size_t _array_len(const void *array);
-size_t _array_cap(const void *array);
-void _array_clear(void *array);
-void _array_clear_ext(void *array, size_t size, Array_Free f);
-void _array_free(void *array);
-void _array_free_ext(void *array, size_t size, Array_Free f);
+void _rl_array_grow(void *array, size_t size, size_t capacity);
+void _rl_array_resize(void *array, size_t size, size_t length);
+void *_rl_array_copy(void *array, size_t size);
+void *_rl_array_push(void *array, size_t size);
+void *_rl_array_pop(void *array, size_t size);
+void *_rl_array_addr(const void *array, size_t size, size_t index);
+size_t _rl_array_len(const void *array);
+size_t _rl_array_cap(const void *array);
+void _rl_array_clear(void *array);
+void _rl_array_clear_ext(void *array, size_t size, RL_Array_Free f);
+void _rl_array_free(void *array);
+void _rl_array_free_ext(void *array, size_t size, RL_Array_Free f);
 
 /*}}}*/
 
