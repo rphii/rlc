71842c7094360a88fb4a0da6f13311b7341633f2
rphii <raphael.seitz18@gmail.com>
2026-03-31 21:21:55 +0000

    in err, fprintf to stderr

--- src/rlc/err.c
+++ src/rlc/err.c
@@ -24,9 +24,9 @@ void rlc_trace_fatal(void) {
     size = backtrace(array, RLC_TRACE_MAX);
     strings = backtrace_symbols(array, size);
     if(strings) {
-        printf(F("*", FG_RD_B BG_BK BOLD) " Obtained %d stack frames: (begin of trace)\n", size);
+        fprintf(stderr, F("*", FG_RD_B BG_BK BOLD) " Obtained %d stack frames: (begin of trace)\n", size);
         for(i = 0; i < size; i++) {
-            printf(F("*", FG_RD_B BG_BK BOLD) " %-3u -> %s\n", i, strings[i]);
+            fprintf(stderr, F("*", FG_RD_B BG_BK BOLD) " %-3u -> %s\n", i, strings[i]);
         }
         free(strings);
     }
@@ -35,15 +35,15 @@ void rlc_trace_fatal(void) {
         struct tm *tm = localtime(&tp.tv_sec);
         char buffer[32];
         strftime(buffer, sizeof(buffer), "%Y-%m-%d %H:%M:%S", tm);
-        printf(F("%s", FG_RD_B BG_BK BOLD), buffer);
+        fprintf(stderr, F("%s", FG_RD_B BG_BK BOLD), buffer);
     } else {
-        printf(F("!", FG_RD_B BG_BK BOLD));
+        fprintf(stderr, F("!", FG_RD_B BG_BK BOLD));
     }
     switch(tp.tv_sec % 4) {
-        case 0: printf(" Bailing out, sorry. (end of trace)\n"); break;
-        case 1: printf(" Giving up, sorry. (end of trace)\n"); break;
-        case 2: printf(" Sincerely, yours truly. (end of trace)\n"); break;
-        default: printf(" Try rebooting, please. (end of trace)\n"); break;
+        case 0: fprintf(stderr, " Bailing out, sorry. (end of trace)\n"); break;
+        case 1: fprintf(stderr, " Giving up, sorry. (end of trace)\n"); break;
+        case 2: fprintf(stderr, " Sincerely, yours truly. (end of trace)\n"); break;
+        default: fprintf(stderr, " Try rebooting, please. (end of trace)\n"); break;
     }
     exit(-1);
 }
